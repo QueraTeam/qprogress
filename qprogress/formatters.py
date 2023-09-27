f0b705cddb69c1c02a0613aa77e295513561b321
Mohammad Jafari <m.jafari9877@gmail.com>
2023-09-27 11:15:32 +0330

    feat: add iteration number to default formatter

--- qprogress/formatters.py
+++ qprogress/formatters.py
@@ -1,4 +1,5 @@
 def default_format_func(current_index: int, total_count: int) -> None:
     percent = (current_index / total_count) * 100
     fill_count = int(percent / 2)
-    print(f"\rProcessing [{'=' * fill_count}{' ' * (50 - fill_count)}] {percent:.1f}%", end="")
+    progress_bar = f"{'=' * fill_count}{' ' * (50 - fill_count)}"
+    print(f"\rProcessing [{current_index} / {total_count}] [{progress_bar}] {percent:.1f}%", end="")
